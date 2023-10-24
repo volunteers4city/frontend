d35fe782bde3db42ca079c61be4044dba587bc7b
DBKai <dbkai@ya.ru>
2023-10-24 15:59:32 +0500

    feat(organizerproject): added props and proptypes

--- src/components/OrganizerProject/OrganizerProject.jsx
+++ src/components/OrganizerProject/OrganizerProject.jsx
@@ -1,16 +1,52 @@
+import PropTypes from 'prop-types';
+import { useRef, useState } from 'react';
 import SelectOption from '../SelectOption/SelectOption';
 import './OrganizerProject.scss';
 import projectImage from '../../images/project-image.png';
 
-function OrganizerProject() {
+function OrganizerProject({
+	name,
+	description,
+	goal,
+	events,
+	tasks,
+	provide,
+	city,
+	address,
+	date,
+	timeRange,
+	submissionDate,
+	categoryProject,
+	skills,
+}) {
+	const [isEditableName, setIsEditableName] = useState(false);
+	const [projectName, setProjectName] = useState(name);
+	const projectNameInput = useRef(null);
+
+	function handleChangeName() {
+		setIsEditableName(!isEditableName);
+		projectNameInput.current.focus();
+	}
+
 	return (
 		<section className="project">
 			<div className="project__name-container">
-				<h1 className="project__name">Название проекта*</h1>
+				<input
+					className="project__input-name"
+					type="text"
+					value={projectName}
+					onChange={(e) => setProjectName(e.target.value)}
+					ref={projectNameInput}
+					readOnly={!isEditableName}
+					onBlur={() => setIsEditableName(false)}
+				/>
+				{/* <h1 className="project__name">{projectName}</h1> */}
+
 				<input
 					className="project__button project__edit-name-button"
 					type="button"
 					aria-label="Редактировать название"
+					onClick={handleChangeName}
 				/>
 			</div>
 
@@ -39,12 +75,16 @@ function OrganizerProject() {
 					<div className="project__group project__group-general">
 						<label className="project__label" htmlFor="project__textarea">
 							Описание проекта*
-							<textarea className="project__textarea" type="text" />
+							<textarea className="project__textarea" type="text" required>
+								{description}
+							</textarea>
 							<span className="project__error-message">Error</span>
 						</label>
 						<label className="project__label" htmlFor="project__textarea">
 							Цель проекта*
-							<textarea className="project__textarea" type="text" />
+							<textarea className="project__textarea" type="text" required>
+								{goal}
+							</textarea>
 							<span className="project__error-message">Error</span>
 						</label>
 						<label className="project__label" htmlFor="project__textarea">
@@ -53,7 +93,9 @@ function OrganizerProject() {
 								className="project__textarea"
 								type="text"
 								placeholder="Например: Лекция по экологии; Посадка саженцев;"
-							/>
+							>
+								{events}
+							</textarea>
 							<span className="project__error-message">Error</span>
 						</label>
 						<label className="project__label" htmlFor="project__textarea">
@@ -62,7 +104,10 @@ function OrganizerProject() {
 								className="project__textarea"
 								type="text"
 								placeholder="Опишите, какие задачи будут стоять перед волонтёрами: к примеру, «уборка территории» и «высадка деревьев»"
-							/>
+								required
+							>
+								{tasks}
+							</textarea>
 							<span className="project__error-message">Error</span>
 						</label>
 						<label className="project__label" htmlFor="project__textarea">
@@ -71,20 +116,29 @@ function OrganizerProject() {
 								className="project__textarea"
 								type="text"
 								placeholder="Например: саженцы, перчатки, обед"
-							/>
+							>
+								{provide}
+							</textarea>
 							<span className="project__error-message">Error</span>
 						</label>
 					</div>
 
 					<h2 className="project__group-label">Место проведения</h2>
 					<div className="project__group project__group-place">
-						<SelectOption label="Город" placeholder="Выберите город" required />
+						<SelectOption
+							label="Город"
+							placeholder="Выберите город"
+							value={city}
+							required
+						/>
 						<label className="project__label" htmlFor="project__text-input">
 							Адрес*
 							<input
 								className="project__text-input"
 								type="text"
 								placeholder="Улица, дом, корпус, строение"
+								value={address}
+								required
 							/>
 							<span className="project__error-message">Error</span>
 						</label>
@@ -98,6 +152,8 @@ function OrganizerProject() {
 								className="project__text-input"
 								type="text"
 								placeholder="01.02.2023"
+								value={date}
+								required
 							/>
 							<span className="project__error-message">Error</span>
 						</label>
@@ -107,6 +163,8 @@ function OrganizerProject() {
 								className="project__text-input"
 								type="text"
 								placeholder="10:00 - 16:00"
+								value={timeRange}
+								required
 							/>
 							<span className="project__error-message">Error</span>
 						</label>
@@ -116,6 +174,8 @@ function OrganizerProject() {
 								className="project__text-input"
 								type="text"
 								placeholder="02.10.2023 - 12.10.2023"
+								value={submissionDate}
+								required
 							/>
 							<span className="project__error-message">Error</span>
 						</label>
@@ -126,12 +186,14 @@ function OrganizerProject() {
 						<SelectOption
 							label="Категория проекта"
 							placeholder="Выберите категорию"
+							value={categoryProject}
 							isMulti
 							required
 						/>
 						<SelectOption
 							label="Навыки"
 							placeholder="Выберите навыки"
+							value={skills}
 							isMulti
 							required
 						/>
@@ -156,5 +218,50 @@ function OrganizerProject() {
 		</section>
 	);
 }
+
+OrganizerProject.propTypes = {
+	name: PropTypes.string,
+	description: PropTypes.string,
+	goal: PropTypes.string,
+	events: PropTypes.string,
+	tasks: PropTypes.string,
+	provide: PropTypes.string,
+	city: PropTypes.shape({
+		label: PropTypes.string,
+		value: PropTypes.string,
+	}),
+	address: PropTypes.string,
+	date: PropTypes.instanceOf(Date),
+	timeRange: PropTypes.string,
+	submissionDate: PropTypes.string,
+	categoryProject: PropTypes.arrayOf(
+		PropTypes.shape({
+			label: PropTypes.string,
+			value: PropTypes.string,
+		})
+	),
+	skills: PropTypes.arrayOf(
+		PropTypes.shape({
+			label: PropTypes.string,
+			value: PropTypes.string,
+		})
+	),
+};
+
+OrganizerProject.defaultProps = {
+	name: 'Название проекта*',
+	description: '',
+	goal: '',
+	events: '',
+	tasks: '',
+	provide: '',
+	city: '',
+	address: '',
+	date: null,
+	timeRange: '',
+	submissionDate: '',
+	categoryProject: null,
+	skills: null,
+};
 
 export default OrganizerProject;
